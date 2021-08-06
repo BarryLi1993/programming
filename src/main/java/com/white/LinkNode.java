deeda93ade204eabb2635d47bac2379491007d93
lijiawen <lijiawen@iyunxiao.com>
2021-08-06 08:56:05 +0800

    add link node cycle

--- src/main/java/com/white/LinkNode.java
+++ src/main/java/com/white/LinkNode.java
@@ -117,6 +117,22 @@ public class LinkNode {
             return rootNode;
         }
     }
+
+    public static Boolean hasCycle(Node rootNode) {
+        if (rootNode == null) {
+            return false;
+        }
+        Node fast = rootNode;
+        Node slow = rootNode;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 class Node {
